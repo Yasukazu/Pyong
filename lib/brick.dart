0fc6cd04afd3dc753da2afcee5df33a8283e1c9d
Yasukazu Makino <mkn@yskz.dev>
2022-04-13 22:01:55 +0900

    Brick(..,PlayerColor)

--- lib/brick.dart
+++ lib/brick.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:pong/homePage.dart';
 
 class Brick extends StatelessWidget {
-  final x;
-  final y;
-  final brickWidth;
-  final isEnemy;
-  Brick( this.x, this.y, this.brickWidth, this.isEnemy);
+  final double x;
+  final double y;
+  final double brickWidth;
+  final Color color; //isEnemy;
+  Brick(this.x, this.y, this.brickWidth, this.color);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        alignment: Alignment((2* x +brickWidth)/(2-brickWidth), y),
+        alignment: Alignment((2 * x + brickWidth) / (2 - brickWidth), y),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(10),
           child: Container(
-              alignment: Alignment(0, 0),
-              color: isEnemy?Colors.purple[500]: Colors.pink[300],
-              height: 20,
-              width:MediaQuery.of(context).size.width * brickWidth/ 2,
-              ),
+            alignment: Alignment(0, 0),
+            color: color, // isEnemy ? PlayerColor.enemy : PlayerColor.self, //Colors.purple[500] : Colors.pink[300],
+            height: 20,
+            width: MediaQuery.of(context).size.width * brickWidth / 2,
+          ),
         ));
   }
 }
