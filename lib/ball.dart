d7baadbec6df44d2a0d649deb0dcf7253b79dff2
Yasukazu Makino <mkn@yskz.dev>
2022-04-16 03:27:17 +0900

    fence

--- lib/ball.dart
+++ lib/ball.dart
@@ -25,48 +25,58 @@ class BallPos {
   double get y => _by.x;
   double get dx => _bx.d;
   double get dy => _by.d;
+  double get xf => _bx.fence;
+  double get yf => _by.fence;
   final Bouncer _bx;
   final Bouncer _by;
 
-  BallPos(double x, double y)
-      : _bx = Bouncer(x),
-        _by = Bouncer(y);
+  BallPos(double x, double y, {xf = 1.0, yf = 1.0})
+      : _bx = Bouncer(x, fence: xf),
+        _by = Bouncer(y, fence: yf);
 
   // angle[degree]
-  BallPos.withAngleDivider(double angle, int divider)
-      : _by = Bouncer(1 / divider),
-        _bx = Bouncer(tan(angle / 360 * 2 * 3.14) / divider);
+  BallPos.withAngleDivider(double angle, int divider, {xf = 1.0, yf = 1.0})
+      : _by = Bouncer(1 / divider, fence: yf),
+        _bx = Bouncer(tan(angle / 360 * 2 * pi) / divider, fence: xf);
 
-  /// return: [x, y]
-  List<double> step() {
-    _bx.step();
-    _by.step();
+  /// return: [x._neg, y._neg]
+  List<bool?> step() {
+    var x = _bx.step();
+    var y = _by.step();
     return [x, y];
   }
 }
 
-// between -1 and 1 bouncing number
+// between -fence and fence bouncing number
 class Bouncer {
-  var _x = 0.0;
-  double _d;
+  double _x;
+  //double _d;
+  final double e;
+  bool _neg;
+  final double fence;
   double get x => _x;
-  double get d => _d;
+  double get d => _neg ? -e : e;
 
-  Bouncer(this._d);
+  /// fence > 0
+  Bouncer(d, {x = 0.0, fence = 1})
+      : this.e = d.abs(),
+        _neg = d < 0,
+        _x = x,
+        this.fence = fence;
 
-  /// return: step amount (inc/dec)
-  double step() {
-    var oldX = _x;
-    var a = _x + _d;
-    if (a < -1) {
-      _x = -a - 2;
-      _d = -_d;
-    } else if (a > 1) {
-      _x = 2 - a;
-      _d = -_d;
-    } else {
-      _x = a;
+  /// return: bounced ? _neg : null
+  bool? step() {
+    final a = _x + d;
+    if (a < -fence) {
+      _x = -a - 2 * fence;
+      _neg = false;
+      return _neg;
+    } else if (a > fence) {
+      _x = 2 * fence - a;
+      _neg = true;
+      return _neg;
     }
-    return _x - oldX;
+    _x = a;
+    return null;
   }
 }
